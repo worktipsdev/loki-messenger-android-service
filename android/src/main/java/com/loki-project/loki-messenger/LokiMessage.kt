4bc07668e626c9d56d38402f2861a1d8d0c9416b
Niels Andriesse <andriesseniels@gmail.com>
2019-05-30 15:48:24 +1000

    Stub LokiMessage

---
+++ android/src/main/java/com/loki-project/loki-messenger/LokiMessage.kt
@@ -0,0 +1,38 @@
+package com.`loki-project`.`loki-messenger`
+
+data class LokiMessage(
+    /**
+     * The hex encoded public key of the receiver.
+     */
+    val destination: String,
+    /**
+     * The content of the message.
+     */
+    val data: String,
+    /**
+     * The time to live for the message in milliseconds.
+     */
+    val ttl: Int,
+    /**
+     * Whether this message is a ping.
+     */
+    val isPing: Boolean,
+    /**
+     * When the proof of work was calculated, if applicable (P2P messages don't require proof of work).
+     *
+     * - Note: Expressed as milliseconds since 00:00:00 UTC on 1 January 1970.
+     */
+    internal var timestamp: Int? = null,
+    /**
+     * The base 64 encoded proof of work, if applicable (P2P messages don't require proof of work).
+     */
+    internal var nonce: String? = null
+) {
+
+    companion object {
+
+        fun from(signalMessage: Map<*, *>): LokiMessage? {
+            return null
+        }
+    }
+}
