7bb6f1920b5ad74cc4fdbd6ca5d94e280c2db7fd
Niels Andriesse <andriesseniels@gmail.com>
2019-05-31 21:10:32 +1000

    Clean

--- java/src/main/java/org/whispersystems/signalservice/loki/extensions/AnyExtension.kt
+++ java/src/main/java/org/whispersystems/signalservice/loki/extensions/AnyExtension.kt
@@ -3,14 +3,8 @@ package org.whispersystems.signalservice.loki.extensions
 import kotlin.reflect.KProperty1
 import kotlin.reflect.full.memberProperties
 
-/**
- * Get a private property from an instance
- * @receiver Any The property type
- * @param name String The name of the property
- * @return R The property
- */
 @Suppress("UNCHECKED_CAST")
-fun <R> Any.getPrivateProperty(name: String): R {
-    val property = this::class.memberProperties.first { it.name == name } as KProperty1<Any, *>
-    return property.get(this) as R
+fun <T : Any, U> T.getProperty(name: String): U {
+    val p = this::class.memberProperties.first { it.name == name } as KProperty1<T, U>
+    return p.get(this)
 }
