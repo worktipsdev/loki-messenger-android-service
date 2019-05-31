8caa217aab2aba4dd75faf8dd89051f3184f471f
Mikunj <mikunj@live.com.au>
2019-05-31 15:55:38 +1000

    Extended LokiServiceCipher to handle decryption.

--- java/src/main/java/org/whispersystems/signalservice/loki/LokiServiceCipher.kt
+++ java/src/main/java/org/whispersystems/signalservice/loki/LokiServiceCipher.kt
@@ -1,16 +1,21 @@
 package org.whispersystems.signalservice.loki
 
+import org.whispersystems.libsignal.InvalidMessageException
 import org.whispersystems.libsignal.SignalProtocolAddress
 import org.whispersystems.libsignal.protocol.CiphertextMessage
 import org.whispersystems.libsignal.state.SignalProtocolStore
 import org.whispersystems.signalservice.api.crypto.SignalServiceCipher
+import org.whispersystems.signalservice.api.messages.SignalServiceEnvelope
 import org.whispersystems.signalservice.api.push.SignalServiceAddress
 import org.whispersystems.signalservice.internal.push.OutgoingPushMessage
 import org.whispersystems.signalservice.internal.push.PushTransportDetails
 import org.whispersystems.signalservice.internal.util.Base64
 
 /**
- * This is just an extension class to `SignalServiceCipher`
+ * This is just an extension class to `SignalServiceCipher`.
+ * Anything that `SignalServiceCipher` does, this class will also do it.
+ *
+ * The only change that has been made is adding custom `decrypt` logic
  */
 class LokiServiceCipher(
         private val localAddress: SignalServiceAddress,
@@ -20,7 +25,7 @@ class LokiServiceCipher(
     /**
      * Encrypt a message with the FallBackSessionCipher
      * @param destination SignalProtocolAddress The destination
-     * @param unpaddedMessage ByteArray The unpadded message
+     * @param unpaddedMessage ByteArray The un-padded message
      * @return OutgoingPushMessage The outgoing message
      */
     fun encryptWithFallbackCipher(destination: SignalProtocolAddress, unpaddedMessage: ByteArray): OutgoingPushMessage {
@@ -31,5 +36,30 @@ class LokiServiceCipher(
 
         // TODO: Replace `WHISPER_TYPE` with our own friend request message type
         return OutgoingPushMessage(CiphertextMessage.WHISPER_TYPE, destination.deviceId, 0, body)
+    }
+
+    /**
+     * Decrypt the envelope.
+     * If the envelope is a `FriendRequest` then we decrypt using `FallBackSessionCipher`.
+     * Otherwise we fallback to using the Signal implementation.
+     */
+    override fun decrypt(envelope: SignalServiceEnvelope, ciphertext: ByteArray): Plaintext {
+        return if (envelope.isFriendRequest) decryptFriendRequest(envelope, ciphertext) else super.decrypt(envelope, ciphertext)
+    }
+
+    /**
+     * Decrypt the friend request using `FallBackSessionCipher`
+     */
+    private fun decryptFriendRequest(envelope: SignalServiceEnvelope, ciphertext: ByteArray): Plaintext {
+        val sourceAddress = SignalProtocolAddress(envelope.source, envelope.sourceDevice)
+        val fallBackCipher = FallBackSessionCipher(signalProtocolStore, sourceAddress)
+
+        // Decrypt and un-pad
+        val paddedMessage = fallBackCipher.decrypt(ciphertext) ?: throw InvalidMessageException("Failed to decrypt friend request message using FallBackSessionCipher.")
+        val transportDetails = PushTransportDetails(fallBackCipher.getSessionVersion())
+        val data = transportDetails.getStrippedPaddingMessageBody(paddedMessage)
+
+        val metadata = Metadata(envelope.source, envelope.sourceDevice, envelope.timestamp, false)
+        return Plaintext(metadata, data)
     }
 }
