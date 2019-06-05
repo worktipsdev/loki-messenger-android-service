b5632f17b1fbcef8494dd6ac696e16bcad185691
Niels Andriesse <andriesseniels@gmail.com>
2019-06-05 14:57:49 +1000

    Don't prefix public keys with "05"

--- java/src/main/java/org/whispersystems/signalservice/loki/utilities/Serialization.kt
+++ java/src/main/java/org/whispersystems/signalservice/loki/utilities/Serialization.kt
@@ -25,10 +25,7 @@ private fun String.convertHexStringToByteArray(): ByteArray {
 }
 
 val IdentityKeyPair.hexEncodedPublicKey: String
-    get() {
-        // Prefixing with "05" is necessary for what seems to be a sort of Signal public key versioning system
-        return "05" + publicKey.serialize().joinToString("") { String.format("%02X", it) }
-    }
+    get() = publicKey.serialize().joinToString("") { String.format("%02X", it) }
 
 val IdentityKeyPair.hexEncodedPrivateKey: String
     get() = privateKey.serialize().joinToString("") { String.format("%02X", it) }
