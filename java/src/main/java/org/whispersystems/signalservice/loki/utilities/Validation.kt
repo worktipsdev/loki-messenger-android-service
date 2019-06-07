f05d77c79f3b0e2e314fb350fa7d31b899005ebd
Niels Andriesse <andriesseniels@gmail.com>
2019-06-07 11:55:54 +1000

    Implement public key validation

---
+++ java/src/main/java/org/whispersystems/signalservice/loki/utilities/Validation.kt
@@ -0,0 +1,12 @@
+package org.whispersystems.signalservice.loki.utilities
+
+object PublicKeyValidation {
+
+    fun isValid(candidate: String): Boolean {
+        val hexCharacters = "0123456789ABCDEF".toSet()
+        val isValidHexEncoding = hexCharacters.containsAll(candidate.toSet())
+        val hasValidLength = candidate.length == 66
+        val hasValidPrefix = candidate.startsWith("05")
+        return isValidHexEncoding && hasValidLength && hasValidPrefix
+    }
+}
