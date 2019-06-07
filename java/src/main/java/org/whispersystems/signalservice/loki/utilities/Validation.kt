c4a9848df339bd2777f83f191cba98a1efe7889b
Niels Andriesse <andriesseniels@gmail.com>
2019-06-07 13:15:55 +1000

    Add missing toUpperCase()

--- java/src/main/java/org/whispersystems/signalservice/loki/utilities/Validation.kt
+++ java/src/main/java/org/whispersystems/signalservice/loki/utilities/Validation.kt
@@ -5,7 +5,7 @@ object PublicKeyValidation {
     @JvmStatic
     fun isValid(candidate: String): Boolean {
         val hexCharacters = "0123456789ABCDEF".toSet()
-        val isValidHexEncoding = hexCharacters.containsAll(candidate.toSet())
+        val isValidHexEncoding = hexCharacters.containsAll(candidate.toUpperCase().toSet())
         val hasValidLength = candidate.length == 66
         val hasValidPrefix = candidate.startsWith("05")
         return isValidHexEncoding && hasValidLength && hasValidPrefix
