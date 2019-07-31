ee409a575724a13822e5a9019add17aab218e172
Niels Andriesse <andriesseniels@gmail.com>
2019-07-31 15:47:47 +1000

    Use lowercase for hex encoding

--- java/src/main/java/org/whispersystems/signalservice/loki/utilities/Serialization.kt
+++ java/src/main/java/org/whispersystems/signalservice/loki/utilities/Serialization.kt
@@ -3,7 +3,7 @@ package org.whispersystems.signalservice.loki.utilities
 import org.whispersystems.libsignal.IdentityKeyPair
 
 val IdentityKeyPair.hexEncodedPublicKey: String
-    get() = publicKey.serialize().joinToString("") { String.format("%02X", it) }
+    get() = publicKey.serialize().joinToString("") { String.format("%02x", it) }
 
 val IdentityKeyPair.hexEncodedPrivateKey: String
-    get() = privateKey.serialize().joinToString("") { String.format("%02X", it) }
+    get() = privateKey.serialize().joinToString("") { String.format("%02x", it) }
