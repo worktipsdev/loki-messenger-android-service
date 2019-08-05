b39f2bd29554f66c45495350cfce1f526fe631cf
Niels Andriesse <andriesseniels@gmail.com>
2019-08-05 10:22:10 +1000

    Parse LokiGroupChatAPI.getMessages() response

--- java/src/main/java/org/whispersystems/signalservice/loki/api/LokiGroupChatAPI.kt
+++ java/src/main/java/org/whispersystems/signalservice/loki/api/LokiGroupChatAPI.kt
@@ -1,5 +1,6 @@
 package org.whispersystems.signalservice.loki.api
 
+import nl.komponents.kovenant.Promise
 import nl.komponents.kovenant.deferred
 import okhttp3.*
 import org.whispersystems.libsignal.logging.Log
@@ -14,13 +15,13 @@ internal class LokiGroupChatAPI {
         private val batchCount = 20
     }
 
-    public fun getMessages(channelID: String): RawResponsePromise {
+    public fun getMessages(channelID: String): Promise<List<LokiGroupMessage>, Exception> {
         Log.d("Loki", "Getting messages for group chat with ID: $channelID.")
         val queryParameters = "include_annotations=1&count=-$batchCount"
         val url = "$serverURL/channels/$channelID/messages?$queryParameters"
         val request = Request.Builder().url(url).get()
         val connection = OkHttpClient()
-        val deferred = deferred<RawResponse, Exception>()
+        val deferred = deferred<List<LokiGroupMessage>, Exception>()
         connection.newCall(request.build()).enqueue(object : Callback {
 
             override fun onResponse(call: Call, response: Response) {
@@ -28,7 +29,20 @@ internal class LokiGroupChatAPI {
                     200 -> {
                         val bodyAsString = response.body()!!.string()
                         @Suppress("NAME_SHADOWING") val body = JsonUtil.fromJson(bodyAsString, Map::class.java)
-                        deferred.resolve(body)
+                        val messagesAsJSON = body["data"] as List<*> // Intentionally throws so that the promise fails if this is missing
+                        val messages = messagesAsJSON.mapNotNull { messageAsJSON ->
+                            val x1 = messageAsJSON as? Map<*, *> ?: return@mapNotNull null
+                            val x2 = x1["annotations"] as? List<*> ?: return@mapNotNull null
+                            val x3 = x2.firstOrNull() as? Map<*, *> ?: return@mapNotNull null
+                            val x4 = x3["value"] as? Map<*, *> ?: return@mapNotNull null
+                            val id = x4["id"] as? String ?: return@mapNotNull null
+                            val hexEncodedPublicKey = x4["source"] as? String ?: return@mapNotNull null
+                            val displayName = x4["from"] as? String ?: return@mapNotNull null
+                            @Suppress("NAME_SHADOWING") val body = x1["text"] as? String ?: return@mapNotNull null
+                            val timestamp = x4["timestamp"] as? Long ?: return@mapNotNull null
+                            LokiGroupMessage(id, hexEncodedPublicKey, displayName, body, timestamp)
+                        }
+                        deferred.resolve(messages)
                     }
                     else -> {
                         Log.d("Loki", "Couldn't reach group chat server.")
@@ -44,23 +58,19 @@ internal class LokiGroupChatAPI {
         return deferred.promise
     }
 
-    public fun sendMessage(message: LokiGroupMessage, channelID: String): RawResponsePromise {
+    public fun sendMessage(message: LokiGroupMessage, channelID: String): Promise<Unit, Exception> {
         Log.d("Loki", "Sending message to group chat with ID: $channelID.")
         val url = "$serverURL/channels/$channelID/messages"
         val parameters = message.toJSON()
         val body = RequestBody.create(MediaType.get("application/json"), parameters)
         val request = Request.Builder().url(url).post(body)
         val connection = OkHttpClient()
-        val deferred = deferred<RawResponse, Exception>()
+        val deferred = deferred<Unit, Exception>()
         connection.newCall(request.build()).enqueue(object : Callback {
 
             override fun onResponse(call: Call, response: Response) {
                 when (response.code()) {
-                    200 -> {
-                        val bodyAsString = response.body()!!.string()
-                        @Suppress("NAME_SHADOWING") val body = JsonUtil.fromJson(bodyAsString, Map::class.java)
-                        deferred.resolve(body)
-                    }
+                    200 -> deferred.resolve(Unit)
                     else -> {
                         Log.d("Loki", "Couldn't reach group chat server.")
                         deferred.reject(LokiAPI.Error.Generic)
--- java/src/main/java/org/whispersystems/signalservice/loki/api/LokiGroupMessage.kt
+++ java/src/main/java/org/whispersystems/signalservice/loki/api/LokiGroupMessage.kt
@@ -1,11 +1,15 @@
 package org.whispersystems.signalservice.loki.api
 
 internal data class LokiGroupMessage(
+        internal val id: String?,
         internal val hexEncodedPublicKey: String,
         internal val displayName: String,
         internal val body: String,
         internal var timestamp: Long
 ) {
+
+    constructor(hexEncodedPublicKey: String, displayName: String, body: String, timestamp: Long)
+        : this(null, hexEncodedPublicKey, displayName, body, timestamp)
 
     internal fun toJSON(): String {
         val intermediate = "{ \"timestamp\" : $timestamp, \"from\" : \"$displayName\", \"source\" : \"$hexEncodedPublicKey\" }"
