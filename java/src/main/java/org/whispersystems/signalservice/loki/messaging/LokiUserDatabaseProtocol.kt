c5555575d9e8c49bd2a5546fc7edf4eed07fed68
Mikunj <mikunj@live.com.au>
2019-08-23 13:23:35 +1000

    Fix variable naming.

--- java/src/main/java/org/whispersystems/signalservice/loki/messaging/LokiUserDatabaseProtocol.kt
+++ java/src/main/java/org/whispersystems/signalservice/loki/messaging/LokiUserDatabaseProtocol.kt
@@ -3,6 +3,6 @@ package org.whispersystems.signalservice.loki.messaging
 interface LokiUserDatabaseProtocol {
 
     fun getDisplayName(hexEncodedPublicKey: String): String?
-    fun getToken(serverUrl: String): String?
-    fun setToken(token: String, serverUrl: String)
+    fun getToken(server: String): String?
+    fun setToken(token: String, server: String)
 }
