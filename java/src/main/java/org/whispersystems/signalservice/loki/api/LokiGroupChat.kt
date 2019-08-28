4fc802394d1b505131e761afa807a03c0a1eb5b9
Niels Andriesse <andriesseniels@gmail.com>
2019-08-28 11:46:03 +1000

    Fix LokiGroupChat ID

--- java/src/main/java/org/whispersystems/signalservice/loki/api/LokiGroupChat.kt
+++ java/src/main/java/org/whispersystems/signalservice/loki/api/LokiGroupChat.kt
@@ -1,9 +1,13 @@
 package org.whispersystems.signalservice.loki.api
 
 public data class LokiGroupChat(
-        public val id: String,
-        public val serverID: Long,
-        public val server: String,
-        public val displayName: String,
-        public val isDeletable: Boolean
-)
+    public val id: String,
+    public val serverID: Long,
+    public val server: String,
+    public val displayName: String,
+    public val isDeletable: Boolean
+) {
+
+    constructor(serverID: Long, server: String, displayName: String, isDeletable: Boolean)
+        : this("$server.$serverID", serverID, server, displayName, isDeletable)
+}
