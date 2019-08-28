30e813c52fb919c04b6ea2fab548953823eac54a
Niels Andriesse <andriesseniels@gmail.com>
2019-08-28 13:23:51 +1000

    Clean

--- java/src/main/java/org/whispersystems/signalservice/loki/api/LokiRSSFeed.kt
+++ java/src/main/java/org/whispersystems/signalservice/loki/api/LokiRSSFeed.kt
@@ -2,7 +2,7 @@ package org.whispersystems.signalservice.loki.api
 
 public data class LokiRSSFeed(
     public val id: String,
-    public val server: String,
+    public val url: String,
     public val displayName: String,
     public val isDeletable: Boolean
 )
