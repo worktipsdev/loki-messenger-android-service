e42341e1001ce6dcd499f0785d284b8fac334ddc
Niels Andriesse <andriesseniels@gmail.com>
2019-10-10 10:49:19 +1100

    Implement mentions API

--- java/src/main/java/org/whispersystems/signalservice/loki/messaging/LokiUserDatabaseProtocol.kt
+++ java/src/main/java/org/whispersystems/signalservice/loki/messaging/LokiUserDatabaseProtocol.kt
@@ -3,4 +3,5 @@ package org.whispersystems.signalservice.loki.messaging
 interface LokiUserDatabaseProtocol {
 
     fun getDisplayName(hexEncodedPublicKey: String): String?
+    fun getServerDisplayName(serverID: String, hexEncodedPublicKey: String): String?
 }
