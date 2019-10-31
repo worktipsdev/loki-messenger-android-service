943b26dfbf3c9536b4cac83139ba9deae44f921d
Mikunj <mikunj@live.com.au>
2019-10-31 13:19:20 +1100

    Added helper function to run a success callback in a background thread.

--- java/src/main/java/org/whispersystems/signalservice/loki/utilities/Promise.kt
+++ java/src/main/java/org/whispersystems/signalservice/loki/utilities/Promise.kt
@@ -2,11 +2,24 @@
 package org.whispersystems.signalservice.loki.utilities
 
 import nl.komponents.kovenant.Promise
+import org.whispersystems.libsignal.logging.Log
 
 fun <V, E: Throwable> Promise<V, E>.get(defaultValue: V): V {
   return try {
-    this.get()
+    get()
   } catch (e: Exception) {
     defaultValue
   }
+}
+
+fun <V, E> Promise<V, E>.successBackground(callback: (value: V) -> Unit): Promise<V, E> {
+  Thread {
+    try {
+      callback(get())
+    } catch (e: Exception) {
+      Log.w("Promise", "Failed to execute task in background: ${e.message}")
+    }
+  }.start()
+
+  return this
 }
