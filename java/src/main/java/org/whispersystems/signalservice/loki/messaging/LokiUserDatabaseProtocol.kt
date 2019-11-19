3e925df49abd6a4bb58bad9d8082882cca655732
ryanzhao <ryanzhaors@qq.com>
2019-11-19 17:03:55 +1100

    try to store the avatar url locally

--- java/src/main/java/org/whispersystems/signalservice/loki/messaging/LokiUserDatabaseProtocol.kt
+++ java/src/main/java/org/whispersystems/signalservice/loki/messaging/LokiUserDatabaseProtocol.kt
@@ -4,4 +4,5 @@ interface LokiUserDatabaseProtocol {
 
     fun getDisplayName(hexEncodedPublicKey: String): String?
     fun getServerDisplayName(serverID: String, hexEncodedPublicKey: String): String?
+    fun getProfileAvatarUrl(hexEncodedPublicKey: String): String?
 }
