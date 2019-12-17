b6e1410f7f921db177eb032792d3b639ca1154e1
Niels Andriesse <andriesseniels@gmail.com>
2019-12-17 10:04:25 +0100

    Add ECKeyPair utilities

--- java/src/main/java/org/whispersystems/signalservice/loki/utilities/Serialization.kt
+++ java/src/main/java/org/whispersystems/signalservice/loki/utilities/Serialization.kt
@@ -1,9 +1,16 @@
 package org.whispersystems.signalservice.loki.utilities
 
 import org.whispersystems.libsignal.IdentityKeyPair
+import org.whispersystems.libsignal.ecc.ECKeyPair
 
 val IdentityKeyPair.hexEncodedPublicKey: String
     get() = publicKey.serialize().joinToString("") { String.format("%02x", it) }
 
 val IdentityKeyPair.hexEncodedPrivateKey: String
+    get() = privateKey.serialize().joinToString("") { String.format("%02x", it) }
+
+val ECKeyPair.hexEncodedPublicKey: String
+    get() = publicKey.serialize().joinToString("") { String.format("%02x", it) }
+
+val ECKeyPair.hexEncodedPrivateKey: String
     get() = privateKey.serialize().joinToString("") { String.format("%02x", it) }
